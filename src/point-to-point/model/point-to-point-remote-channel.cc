9bdb9e4a0854f89755f935915e20baad0bfb1753
Yunnai <1556605435@qq.com>
2026-03-18 10:07:36 +0800

    feat: Port simulation files to UNISON

--- src/point-to-point/model/point-to-point-remote-channel.cc
+++ src/point-to-point/model/point-to-point-remote-channel.cc
@@ -11,7 +11,9 @@
 #include "point-to-point-net-device.h"
 
 #include "ns3/log.h"
+#ifdef NS3_MPI
 #include "ns3/mpi-interface.h"
+#endif
 #include "ns3/packet.h"
 #include "ns3/simulator.h"
 
@@ -56,10 +58,15 @@ PointToPointRemoteChannel::TransmitStart(Ptr<const Packet> p,
     uint32_t wire = src == GetSource(0) ? 0 : 1;
     Ptr<PointToPointNetDevice> dst = GetDestination(wire);
 
+#ifdef NS3_MPI
     // Calculate the rxTime (absolute)
     Time rxTime = Simulator::Now() + txTime + GetDelay();
     MpiInterface::SendPacket(p->Copy(), rxTime, dst->GetNode()->GetId(), dst->GetIfIndex());
     return true;
+#else
+    NS_FATAL_ERROR("Can't use distributed simulator without MPI compiled in");
+    return false;
+#endif
 }
 
 } // namespace ns3
